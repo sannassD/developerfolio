b84b8ad63cd6c8558c7df796588d488f87f88cb9
Assane Dia <119739327+sannassD@users.noreply.github.com>
2026-03-07 03:21:35 +0000

    Translate menu items to French

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -43,12 +43,12 @@ function Header() {
         <ul className={isDark ? "dark-menu menu" : "menu"}>
           {viewSkills && (
             <li>
-              <a href="#skills">Skills</a>
+              <a href="#skills">Compétences</a>
             </li>
           )}
           {viewExperience && (
             <li>
-              <a href="#experience">Work Experiences</a>
+              <a href="#experience">Expériences professionnelles</a>
             </li>
           )}
           {viewOpenSource && (
