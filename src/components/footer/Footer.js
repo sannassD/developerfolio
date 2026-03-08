f6bc610efdd85391c52b34e261033c1891fed3a3
Assane Dia <119739327+sannassD@users.noreply.github.com>
2026-03-08 01:49:41 +0000

    Remove emoji from footer
    Remove emoji from footer text.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji(" ❤️ by Assane Dia")}
+          {emoji(" ")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
